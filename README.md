c413c8c87fa2adbceba16ed7cd0a3ce1a86270eb
Brandon Pereira <brandonp@criticalmass.com>
2018-03-20 10:17:19 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# Text Scroll
+
+Zero-dependency module to scroll between predefined text. Leverages Element.animate for modern browsers, and falls back on simple class based animations for legacy browsers.
+
+![Example](https://github.com/brandon-pereira/example.gif)
+
+## Installation
+
+This project is meant to be used with a dependency manager (Webpack, Browserify, etc). You should npm install it, then you can access it in your front end code.
+
+```bash
+npm install scroll-text --save
+```
+
+## Usage
+
+To bind it to any element with a [data-scroll-text] attribute, use this code.
+
+```javascript
+import ScrollText from 'scroll-text';
+
+const els = document.querySelectorAll('[data-scroll-text]);
+Array.from(els).forEach(el => {
+    new ScrollText(el);
+});
+```
+
+To pass options:
+
+```javascript
+import ScrollText from 'scroll-text';
+new ScrollText(document.querySelector('#element'), {
+    speed: 500
+});
+```
+
+## Methods
+
+ScrollText has very minimal public methods:
+
+- constructor `const instance = new ScrollText(el, options)`
+  - `el` (HTMLElement) element to activate ScrollText on
+  - `options` (Object) any additional options to add. Options include:
+    - `speed` [Integer] Time to wait before changing text (in ms). Default: `2000`.
+    - `textAttribute` [String] What attribute to look for. ie data-scroll-text="hello|world|i'm|brandon". Default: `data-scroll-text`.
+    - `scrollAmount` [String] How many pixels to move the text when scrolling.  Default: `20px`.
+    - `scrollSpeed` [Integer] How fast to scroll text (the animation duration). Default: `200`.
+    - `delimiter` [String] What to break the string on. Default `|`.
+    - `text` [Array] Array of strings that should be shown **instead** of lookng at the `textAttribute` property.
+
+- destroy `instance.destroy()`.
+  - This will destroy component and unbind all listeners. Useful if you need to use this on something that will later be removed (modals, pages, etc.)
+
+- next `instance.next()`
+  - Immediately switch to the next slide.
+
+## Styling Considerations
+
+The following is recommended for styling. These styles ensure the component renders correctly, and the opacity/transform/transition properties are used in browsers that don't support the native Element.animate methods.
+
+```scss
+[data-scroll-text] {
+  display: inline;
+  position: relative;
+  span {
+    transition: all .2s;
+    position: absolute;
+    left: 0;
+    opacity: 0;
+    transform: translateY(50px);
+  }
+  span.current {
+    position: relative;
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+```
+
+## TODO
+
+- Gif of it in use
+- Publish it
---
+++ index.js
@@ -0,0 +1,103 @@
+class ScrollText {
+
+    constructor(el, options) {
+        this.el = el;
+        this.options = Object.assign({}, {
+            textAttribute: 'data-scroll-text', // What attribute to look for. ie data-scroll-text="hello|world|i'm|brandon"
+            speed: 2000, // How often to switch slides
+            scrollAmount: '20px', // how often to scroll
+            delimiter: '|', // what to break on for textAttribute
+            // text: [] // Text to use (rather than getting from attribute)
+        }, options);
+
+        this.init();
+    }
+
+    /**
+     * Method to bootstrap the component.
+     */
+    init() {
+        this.el.innerHTML = ''; // Clear the content (this is for if browser has js disabled)
+        this.text = this.options.text || this.el.getAttribute(this.options.textAttribute).split(this.options.delimiter);
+        this.children = this.text.map((el, i) => this._createTextElement(el, i === 0));
+        this.index = 0;
+        this.currentElement = this.children[this.index];
+        this.timer = setInterval(this.next.bind(this), this.options.speed);
+    }
+
+    /**
+     * Public method to switch to the next available slide.
+     */
+    next() {
+        this.index += 1;
+        if (this.index > this.text.length - 1) {
+            this.index = 0;
+        }
+        this._setCurrentSlide(this.index);
+    }
+
+    /**
+     * Method to switch slide, handles delegating animations and setting styles.
+     * @private
+     * @param {Element} curr currently visible element (pushes it off screen from center)
+     * @param {*} next visible to transition to (pushes it up from bottom)
+     */
+    setCurrentSlide(index) {
+        const prev = this.currentElement;
+        const next = this.children[index];
+        if (prev && next) {
+            prev.style.position = 'absolute';
+            next.style.position = 'relative';
+            this._animate(next, [{ opacity: 0, top: this.options.scrollAmount }, { opacity: 1, top: '0px' }], { fill: "both", duration: 200 });
+            this._animate(prev, [{ opacity: 1, top: '0px' }, { opacity: 0, top: `-${this.options.scrollAmount}` }], { fill: "both", duration: 200 }, () => {
+                // on animation end OR unsupported browser
+                prev.classList.remove('current');
+                next.classList.add('current');
+
+            });
+            this.currentElement = next;
+        } else {
+            console.warn("Invalid index passed into scrollText!", index);
+        }
+    }
+
+    /**
+     * Method to call Element.animate if available.
+     * @param {Element} el Element to animate
+     * @param {Array} transition transition to pass
+     * @param {Object} opts options to pass
+     * @param {Function} Callback on complete (called immediately if not supported)
+     */
+    _animate(el, transition, opts, cb = () => { }) {
+        if (el && 'animate' in el) {
+            el.animate(transition, opts)
+                .onfinish = cb;
+        } else if (cb) {
+            cb(); // dont animate just call callback
+        }
+    }
+
+    /**
+     * Creates and appends a text element to the root node. If index is 0 will make it visible, else will be hidden.
+     * @param {String} text 
+     * @param {Number} index 
+     */
+    _createTextElement(text, isCurrent) {
+        const el = document.createElement('span');
+        el.innerText = text;
+        el.classList.toggle('current', isCurrent);
+        return this.el.appendChild(el)
+    }
+
+    /**
+     * Cleanup method. Call this if the component needs to unmount and be removed.
+     */
+    destroy() {
+        clearInterval(this.timer);
+        this.el.innerHTML = this.text[0] || '';
+    }
+
+
+}
+
+module.exports = ScrollText;
